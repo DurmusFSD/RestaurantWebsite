46e53aec004fd8979b4cd30a72ba54a832476c02
Caso66 <durmusozgul66@gmail.com>
2023-09-22 18:06:54 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <div className="socialMedia">
+          <p> &copy; 2023 burger@gmail.com</p>
+          <i className="fa-brands fa-instagram"></i>
+          <i className="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-tiktok"></i>
+          <i className="fa-solid fa-burger"></i>
+        </div>
+      </div> 
+
+
+
+    </>
+  );
+}
+
+export default Footer;
